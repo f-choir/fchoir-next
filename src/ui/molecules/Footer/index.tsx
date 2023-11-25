015331dd273aa772619305517dba8c61f811d979
Bee Godwin <bee.godwin@bbc.co.uk>
2023-11-25 20:13:39 +0000

    hook up footer urls

--- src/ui/molecules/Footer/index.tsx
+++ src/ui/molecules/Footer/index.tsx
@@ -1,8 +1,8 @@
 import classNames from 'classnames';
 
-const INSTAGRAM = '';
-const EMAIL = '';
-const FACEBOOK = '';
+const INSTAGRAM = 'https://instagram.com/f__choir';
+const EMAIL = 'mailto:wearefchoir@gmail.com';
+const FACEBOOK = 'https://facebook.com/fchoirlondon';
 
 const linkClasses = classNames(
   'text-red text-l l:text-xl text-shadow p-2 m:p-4 pl-4 l:px-8 pt-2 pb-2 m:pb-6',
@@ -15,13 +15,13 @@ const Footer = () => (
       className={linkClasses}
       aria-label={'instagram.com @ f underscore underscore choir'}
     >
-      instagram.com/@f__choir
+      instagram - @f__choir
     </a>
     <a href={EMAIL} className={linkClasses} aria-label={'we are f choir @ g mail.com'}>
       wearefchoir@gmail.com
     </a>
     <a href={FACEBOOK} className={linkClasses} aria-label={'facebook dot com f choir london'}>
-      facebook.com/fchoirlondon
+      facebook - fchoirlondon
     </a>
   </footer>
 );
