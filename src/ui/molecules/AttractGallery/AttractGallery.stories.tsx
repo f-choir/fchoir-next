eac4efdc89409271c7a81be50997c7297a20054b
Bee Godwin <bee.godwin@bbc.co.uk>
2024-01-03 17:53:50 +0000

    trailing slash paths

--- src/ui/molecules/AttractGallery/AttractGallery.stories.tsx
+++ src/ui/molecules/AttractGallery/AttractGallery.stories.tsx
@@ -10,5 +10,5 @@ export default meta;
 type Story = StoryObj<typeof AttractGallery>;
 
 export const Main: Story = {
-  render: () => <AttractGallery />,
+  render: () => <AttractGallery id={'test-gallery'} interval={2000} />,
 };
