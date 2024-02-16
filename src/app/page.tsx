ae002042943e89b30ac789ec0fb10130188033a8
Bee Godwin <bee.godwin@bbc.co.uk>
2024-02-16 14:37:43 +0000

    omg does this basically work now? sweeet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,14 @@ import AttractGallery from '@/ui/molecules/AttractGallery/AttractGallery';
 
 export default function Home() {
   return (
-    <main className="min-h-screen pt-12 m:pt-8">
+    <main className="pt-12 m:pt-8">
       <div className={'absolute top-24 m:top-28 left-1/5 m:left-4/5 l:left-1/3 xl:left-1/2'}>
         <Headline text={'f*choir'} wrapClasses={'relative z-20'} />
       </div>
       <div className={'pt-20'}>
         <AttractGallery id={'test-gallery'} interval={2000} />
         <SubHeader
-          className={'text-center -mt-16 mb-8'}
+          className={'relative top-0 m:-top-16 l:-top-24 text-center mb-8'}
           text={'causing a racket // singing together'}
         />
       </div>
--- src/ui/atoms/Grid/index.tsx
+++ src/ui/atoms/Grid/index.tsx
@@ -1,11 +1,19 @@
 import { PropsWithChildren } from 'react';
 import Wrap from '@/ui/atoms/Wrap';
+import classNames from 'classnames';
 
 interface GridProps {}
 
 const Grid = ({ children }: PropsWithChildren<GridProps>) => (
   <Wrap className={'flex flex-row justify-center'}>
-    <div className={'grid grid-cols-1 m:grid-cols-3 gap-4 gap-y-4'}>{children}</div>
+    <div
+      className={classNames(
+        'grid grid-cols-1 m:grid-cols-2 l:grid-cols-3 gap-4 gap-y-4',
+        'mt-8 m:mt-4 l:mt-2 mb-0 m:mb-8 l:mb-16',
+      )}
+    >
+      {children}
+    </div>
   </Wrap>
 );
 
