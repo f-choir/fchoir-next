0de259fb6a97257ada1e0ae16afafaea7a68301f
Bee Godwin <bee.godwin@bbc.co.uk>
2024-04-26 17:40:08 +0100

    jwt is stored in local, password form only shows if logged out

--- packages/number-one-fan/src/app/members/page.tsx
+++ packages/number-one-fan/src/app/members/page.tsx
@@ -16,45 +16,51 @@ export default function Members() {
   const { data } = useQuery({
     queryKey: ['hello-world'],
     queryFn: async () => {
-      const login = await postAuth(user);
-      const data = await firstNewsPost(login.jwt ?? undefined);
+      if (!window.localStorage.getItem('f-choir-tkn')) {
+        const login = await postAuth(user);
+        login.jwt && window.localStorage.setItem('f-choir-tkn', login.jwt);
+      }
+      const jwt = window.localStorage.getItem('f-choir-tkn');
+      const data = await firstNewsPost(jwt ?? undefined);
       const strapi = await data.json();
       return {
         title: strapi.data.attributes.Title,
         body: strapi.data.attributes.Body[0].children[0].text,
       };
     },
+    staleTime: 24 * 60 * 60 * 1000,
   });
 
   return (
     <>
       <Headline text={'f*members'} />
       <Wrap>
-        <p>Authenticated API Request to Strapi</p>
-        <form onSubmit={() => false}>
-          <label htmlFor="user">user</label>
-          <input
-            type="text"
-            name="user"
-            value={userField}
-            onChange={(e) => setUserField(e.target.value)}
-          />
-          <label htmlFor="password">password</label>
-          <input
-            type="password"
-            name="password"
-            value={passwordField}
-            onChange={(e) => setPasswordField(e.target.value)}
-          />
-          <input
-            type="button"
-            value="go"
-            onClick={() => {
-              setUser({ identifier: userField, password: passwordField });
-              queryClient.invalidateQueries({ queryKey: ['hello-world'] });
-            }}
-          />
-        </form>
+        {!window.localStorage.getItem('f-choir-tkn') && (
+          <form onSubmit={() => false}>
+            <label htmlFor="user">user</label>
+            <input
+              type="text"
+              name="user"
+              value={userField}
+              onChange={(e) => setUserField(e.target.value)}
+            />
+            <label htmlFor="password">password</label>
+            <input
+              type="password"
+              name="password"
+              value={passwordField}
+              onChange={(e) => setPasswordField(e.target.value)}
+            />
+            <input
+              type="button"
+              value="go"
+              onClick={() => {
+                setUser({ identifier: userField, password: passwordField });
+                queryClient.invalidateQueries({ queryKey: ['hello-world'] });
+              }}
+            />
+          </form>
+        )}
         {data && (
           <p>
             <span className="font-bold">{`${data.title} `}</span>
