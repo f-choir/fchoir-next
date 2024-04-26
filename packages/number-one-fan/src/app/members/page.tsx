140d1109acc09a4730ab660350b5c3744bfa03e7
Bee Godwin <bee.godwin@bbc.co.uk>
2024-04-26 15:15:28 +0100

    members page integrates react-query

--- packages/number-one-fan/src/app/members/page.tsx
+++ packages/number-one-fan/src/app/members/page.tsx
@@ -1,31 +1,40 @@
+'use client';
 import Headline from '@/ui/atoms/Headline';
 import Wrap from '@/ui/atoms/Wrap';
-import { firstNewsPost } from '@/api/firstNewsPost';
+// import { firstNewsPost } from '@/api/firstNewsPost';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-type News = {
-  title: string;
-  body: string;
-};
+// type News = {
+//   title: string;
+//   body: string;
+// };
 
-const getData = async (): Promise<News> => {
-  const res = await firstNewsPost();
-  const strapi = await res.json();
-  return {
-    title: strapi.data.attributes.Title,
-    body: strapi.data.attributes.Body[0].children[0].text,
-  };
-};
+// const getData = async (): Promise<News> => {
+//   const res = await firstNewsPost();
+//   const strapi = await res.json();
+//   return {
+//     title: strapi.data.attributes.Title,
+//     body: strapi.data.attributes.Body[0].children[0].text,
+//   };
+// };
 
-export default async function Members() {
-  const news = await getData();
+export default function Members() {
+  // const news = await getData();
+
+  const queryClient = new QueryClient();
 
   return (
-    <main className="min-h-screen pt-12 m:pt-8">
-      <Headline text={'f*members'} />
-      <Wrap>
-        <p className="font-bold text-xl">{news.title}</p>
-        <p>{news.body}</p>
-      </Wrap>
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <main className="min-h-screen pt-12 m:pt-8">
+        <Headline text={'f*members'} />
+        <Wrap>
+          {/*<p className="font-bold text-xl">{news.title}</p>*/}
+          {/*<p>{news.body}</p>*/}
+          <p>Authenticated API Request to Strapi</p>
+        </Wrap>
+      </main>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
