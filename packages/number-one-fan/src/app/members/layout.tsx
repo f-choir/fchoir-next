92adac9a1d40af61ab10d24fb8e392f5d970952b
Bee Godwin <bee.godwin@bbc.co.uk>
2024-04-26 15:39:14 +0100

    client side fetch with strapi

---
+++ packages/number-one-fan/src/app/members/layout.tsx
@@ -0,0 +1,14 @@
+'use client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+export default function ClientSideFetchingLayout({ children }: { children: React.ReactNode }) {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <main className="min-h-screen pt-12 m:pt-8">{children}</main>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
--- packages/number-one-fan/src/app/members/page.tsx
+++ packages/number-one-fan/src/app/members/page.tsx
@@ -1,40 +1,34 @@
 'use client';
 import Headline from '@/ui/atoms/Headline';
 import Wrap from '@/ui/atoms/Wrap';
-// import { firstNewsPost } from '@/api/firstNewsPost';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
-// type News = {
-//   title: string;
-//   body: string;
-// };
-
-// const getData = async (): Promise<News> => {
-//   const res = await firstNewsPost();
-//   const strapi = await res.json();
-//   return {
-//     title: strapi.data.attributes.Title,
-//     body: strapi.data.attributes.Body[0].children[0].text,
-//   };
-// };
+import { useQuery } from '@tanstack/react-query';
+import { firstNewsPost } from '@/api/firstNewsPost';
 
 export default function Members() {
-  // const news = await getData();
-
-  const queryClient = new QueryClient();
+  const { data } = useQuery({
+    queryKey: ['hello-world'],
+    queryFn: async () => {
+      const data = await firstNewsPost();
+      const strapi = await data.json();
+      return {
+        title: strapi.data.attributes.Title,
+        body: strapi.data.attributes.Body[0].children[0].text,
+      };
+    },
+  });
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <main className="min-h-screen pt-12 m:pt-8">
-        <Headline text={'f*members'} />
-        <Wrap>
-          {/*<p className="font-bold text-xl">{news.title}</p>*/}
-          {/*<p>{news.body}</p>*/}
-          <p>Authenticated API Request to Strapi</p>
-        </Wrap>
-      </main>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <>
+      <Headline text={'f*members'} />
+      <Wrap>
+        <p>Authenticated API Request to Strapi</p>
+        {data && (
+          <p>
+            <span className="font-bold">{`${data.title} `}</span>
+            {data.body}
+          </p>
+        )}
+      </Wrap>
+    </>
   );
 }
