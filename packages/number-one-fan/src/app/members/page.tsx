54224554ca52425572427603c9a92c1c6aec2c20
Bee Godwin <bee.godwin@bbc.co.uk>
2024-05-10 13:47:17 +0100

    cookie params fixes, refactor

--- packages/number-one-fan/src/app/members/page.tsx
+++ packages/number-one-fan/src/app/members/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Headline from '@/ui/atoms/Headline';
 import Wrap from '@/ui/atoms/Wrap';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { queryOptions, useQuery, useQueryClient } from '@tanstack/react-query';
 import { firstNewsPost } from '@/api/firstNewsPost';
 import { useState } from 'react';
 import { authenticate } from '@/api';
@@ -11,20 +11,22 @@ import LoginForm from '@/ui/molecules/LoginForm';
 export default function Members() {
   const [user, setUser] = useState({ identifier: '', password: '' });
 
-  const { data } = useQuery({
-    queryKey: ['hello-world'],
-    queryFn: async () => {
-      await authenticate(user);
-      const jwt = getCookieNamed('f-choir-tkn');
-      const data = await firstNewsPost(jwt ?? undefined);
-      const strapi = await data.json();
-      return {
-        title: strapi.data.attributes.Title,
-        body: strapi.data.attributes.Body[0].children[0].text,
-      };
-    },
-    staleTime: 24 * 60 * 60 * 1000,
-  });
+  const { data } = useQuery(
+    queryOptions({
+      queryKey: ['hello-world'],
+      queryFn: async () => {
+        const jwt = await authenticate(user);
+        const data = await firstNewsPost(jwt);
+        const strapi = await data.json();
+        return {
+          title: strapi.data.attributes.Title,
+          body: strapi.data.attributes.Body[0].children[0].text,
+        };
+      },
+      staleTime: 24 * 60 * 60 * 1000,
+      // retryDelay: (attempt) => attempt * 60 * 1000,
+    }),
+  );
 
   return (
     <>
@@ -35,7 +37,7 @@ export default function Members() {
         )}
         {data && (
           <p>
-            <span className="font-bold">{`${data.title} `}</span>
+            <span className="font-bold">{`${data.title ? data.title : ''} `}</span>
             {data.body}
           </p>
         )}
