3cf3470e5405ef1aa96454fe7d8c344bd0c12b3b
Bee Godwin <bee.godwin@bbc.co.uk>
2024-05-10 10:22:31 +0100

    add typescript-cookie

--- packages/number-one-fan/src/cookie/index.ts
+++ packages/number-one-fan/src/cookie/index.ts
@@ -1,5 +1,6 @@
-export const getFromLocalStorage = (key: string) => {
+export const getFromLocalStorage = (key: string): string | null => {
   if (typeof window !== 'undefined') return window?.localStorage?.getItem(key);
+  return null;
 };
 export const setToLocalStorage = (key: string, value: string) => {
   if (typeof window !== 'undefined') window?.localStorage?.setItem(key, value);
