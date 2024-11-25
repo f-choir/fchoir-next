d21535b789695a1bbd3104a3ccfb9b5acb8326e0
Bee Godwin <bee.godwin@bbc.co.uk>
2024-11-25 12:38:26 +0000

    modal displays title and description and has link to gallery

--- packages/number-one-fan/src/ui/molecules/AttractGallery/AttractGallery.tsx
+++ packages/number-one-fan/src/ui/molecules/AttractGallery/AttractGallery.tsx
@@ -24,8 +24,6 @@ const AttractGallery = ({ items }: AttractGalleryProps) => {
 
   const updateIsViewingHero = (isViewing: boolean) => setIsViewingHero(isViewing);
 
-  console.log('BEEBUG: items', items);
-
   return (
     <>
       <div className={'h-[60vw] w-[100vw] relative z-0'}>
@@ -40,16 +38,30 @@ const AttractGallery = ({ items }: AttractGalleryProps) => {
       <div
         className={`${
           isViewingHero ? 'inline-block' : 'hidden'
-        } absolute top-24 z-10 w-[100vw] h-[60vw]`}
+        } absolute top-16 z-40 w-[100vw] h-[60vw] bg-dove`}
       >
         <CloseButton onClose={() => updateIsViewingHero(false)} />
-        <Image
-          className={'foo'}
-          alt={'bar'}
-          src={items[viewIdx].imgUrl}
-          fill={true}
-          unoptimized={false}
-        />
+        <div className={'flex'}>
+          <div className={'w-[70vw] h-[45vw] relative m-4'}>
+            <Image
+              className={'object-cover'}
+              alt={'bar'}
+              src={items[viewIdx].imgUrl}
+              fill={true}
+              unoptimized={false}
+            />
+          </div>
+          <div className={'m-4'}>
+            <div className={'text-4xl font-bold'}>{items[viewIdx].galleryTitle}</div>
+            <div className={'text-2xl py-4'}>{items[viewIdx].galleryDescription}</div>
+            <a
+              className={'text-2xl hover:text-red hover:underline transition-all'}
+              href={`gallery/${items[viewIdx].galleryId}`}
+            >
+              More from this event
+            </a>
+          </div>
+        </div>
       </div>
     </>
   );
