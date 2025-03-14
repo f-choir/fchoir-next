1d5c7f6d9a42792c4aa3a85fbda5393653092363
Bee Godwin <bee.godwin@bbc.co.uk>
2025-03-14 09:51:54 +0000

    add support page shell

---
+++ packages/number-one-fan/src/app/support/page.tsx
@@ -0,0 +1,13 @@
+import Headline from '@/ui/atoms/Headline';
+import Wrap from '@/ui/atoms/Wrap';
+
+export default async function Support() {
+  return (
+    <main className="min-h-screen pt-12 m:pt-8">
+      <Headline text={'support us'} />
+      <Wrap>
+        <p>Under construction. do we still say that about webpages now it's not the '90s?</p>
+      </Wrap>
+    </main>
+  );
+}
