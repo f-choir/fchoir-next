03d2bb48a0256b4836b23a39c7cdaa7177b2494d
Bee Godwin <bee.godwin@bbc.co.uk>
2025-03-14 09:55:23 +0000

    escape apostrophe

--- packages/number-one-fan/src/app/support/page.tsx
+++ packages/number-one-fan/src/app/support/page.tsx
@@ -6,7 +6,7 @@ export default async function Support() {
     <main className="min-h-screen pt-12 m:pt-8">
       <Headline text={'support us'} />
       <Wrap>
-        <p>Under construction. do we still say that about webpages now it's not the '90s?</p>
+        <p>Under construction. do we still say that about webpages now it&apos;s not the '90s?</p>
       </Wrap>
     </main>
   );
