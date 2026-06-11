542f73514681b4845bd16989378f3079670b1c2a
Bea Godwin <ayebeecoding@icloud.com>
2026-06-11 22:46:43 +0100

    tidying

--- packages/number-one-fan/src/ui/atoms/MaskedImage/index.tsx
+++ packages/number-one-fan/src/ui/atoms/MaskedImage/index.tsx
@@ -7,15 +7,24 @@ interface MaskedImageProps {
 
 const MaskedImage = ({imageUrl, path, className}: MaskedImageProps) => {
   return (
-    <svg
-      viewBox={'0 0 829.1579360962 576.7244873047'}
-      className={`w-11/12 h-11/12 ${className || ''}`}
-    >
-      <clipPath id="imageMask">
-        <path d={path} />
-      </clipPath>
-      <image href={imageUrl} x="-34%" y="-34%" width="160%" height="160%" clipPath="url(#imageMask)" />
-    </svg>
+    <>
+      <svg
+        viewBox={'0 0 829.1579360962 576.7244873047'}
+        className={`w-11/12 h-11/12 ${className || ''}`}
+      >
+        <clipPath id="imageMask">
+          <path d={path} />
+        </clipPath>
+        <image
+          href={imageUrl}
+          x="-34%"
+          y="-34%"
+          width="160%"
+          height="160%"
+          clipPath="url(#imageMask)"
+        />
+      </svg>
+    </>
   );}
 
 export default MaskedImage;
